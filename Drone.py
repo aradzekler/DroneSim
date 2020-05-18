dc9e3df49f457825900cd411f704912feb8ca69e
aradzekler <mcspancy@gmail.com>
2020-05-18 18:25:42 +0300

    added a manual drone simulator, still in the works.

--- Drone.py
+++ Drone.py
@@ -1,27 +1,49 @@
 import numpy as np
-
+from pygame.math import Vector2
+import math as math
 '''
 class for defining a model.
 
 '''
 
+MAX_VELOCITY = 20
+BRAKE_DEACCELERATION = 10
+FREE_DEACCELERATION = 2
+MAX_STEERING_ANGLE = 30
+
 
 class Model:
 
-    def __init__(self, pos):  # pos -> an [x,y] array of numbers
+    def __init__(self, x, y):  # pos -> an [x,y] array of numbers
         self.mass = 1.0  # total mass [kg]
         self.air_dens = 1.2  # air density [kgm^-3], can defined how aerodynamic the drone is.
         self.power_pre = 1.0  # battery power [power/100]
+        self.angle = 0.0  # angle of drone. in deg.
+        self.velocity = Vector2(0.0, 0.0)  # vertical , horizontal speed.
         self.ro_rad = 0.20  # rotor radius [m]
+        self.length = 0.5  # drone have a diameter of 0.5m
         self.ro_area = np.pi * self.ro_rad ** 2  # rotor total area [m^2]
-        self.x = pos[0]  # x coordinate
-        self.y = pos[1]  # y coordinate
-        self.state = 0  # the drone state, UNDEFINED
+        self.position = Vector2(x, y)
+        self.state = 0  # the drone state, 0 is manual
         self.type = "QUAD"  # drone type.
+        self.steer = 0.0
+        self.acceleration = 0.0
         # self.sensors =
 
-    def update(self, u):
-        self.u = u
+    def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-MAX_VELOCITY, min(self.velocity.x, MAX_VELOCITY))
+
+        if self.state == 0:
+            if self.steer:
+                turn_radius = self.length/math.tan(math.radians(self.steer))
+                angular_velocity = self.velocity.x / turn_radius
+            else:
+                angular_velocity = 0
+
+            self.position += self.velocity.rotate(-self.angle) * dt
+            self.angle += math.degrees(angular_velocity) * dt
+
 
     # method for computing change based on state and user input if exists.
     # def omega(self, state, inp):
--- SimulationWindow.py
+++ SimulationWindow.py
@@ -1,2 +1,79 @@
 import tkinter as tk
+import pygame
+import Drone
+import os
+from math import copysign
 
+FILE_PATH = './.simu'
+
+
+class SimulationWindow:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Drone Simulation")
+        width = 1500
+        height = 1500
+        self.screen = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
+        self.ticks = 60
+        self.exit = False
+
+    def run(self):
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        img_path = os.path.join(curr_dir, "drone.png")
+        drone_img = pygame.image.load(img_path)
+        drone = Drone.Model(0, 0)
+        ppu = 32
+        while not self.exit:
+            dt = self.clock.get_time() / 1000
+
+            for event in pygame.event.get():  # our event que.
+
+                if event.type == pygame.QUIT:
+                    self.exit = True
+
+                # getting user input
+                key_stroke = pygame.key.get_pressed()
+                if key_stroke[pygame.K_UP]:  # the up key
+                    if drone.velocity.x < 0:
+                        drone.acceleration = Drone.BRAKE_DEACCELERATION
+                    drone.acceleration += 1 * dt
+                elif key_stroke[pygame.K_DOWN]:
+                    if drone.velocity.x > 0:
+                        drone.acceleration = -Drone.BRAKE_DEACCELERATION
+                elif key_stroke[pygame.K_SPACE]:
+                    if abs(drone.velocity.x) > dt * Drone.BRAKE_DEACCELERATION:
+                        drone.acceleration = -copysign(Drone.BRAKE_DEACCELERATION, drone.velocity.x)
+                    else:
+                        drone.acceleration = -drone.velocity.x / dt
+                else:
+                    if abs(drone.velocity.x) > dt * Drone.FREE_DEACCELERATION:
+                        drone.acceleration = -copysign(Drone.FREE_DEACCELERATION, drone.velocity.x)
+                    else:
+                        if dt != 0:
+                            drone.acceleration = -drone.velocity.x / dt
+                drone.acceleration = max(-Drone.MAX_VELOCITY, min(drone.acceleration, Drone.MAX_VELOCITY))
+
+                if key_stroke[pygame.K_RIGHT]:
+                    drone.steering -= 30 * dt
+                elif key_stroke[pygame.K_LEFT]:
+                    drone.steering += 30 * dt
+                else:
+                    drone.steering = 0
+                drone.steering = max(-Drone.MAX_STEERING_ANGLE, min(drone.steering, Drone.MAX_STEERING_ANGLE))
+
+                drone.update(dt)
+                # Drawing
+                self.screen.fill((0, 0, 0))
+                rotated = pygame.transform.rotate(drone_img, drone.angle)
+                rect = rotated.get_rect()
+                self.screen.blit(rotated, drone.position * ppu - (rect.width / 2, rect.height / 2))
+                pygame.display.flip()
+
+                self.clock.tick(self.ticks)
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    game = SimulationWindow()
+    game.run()
